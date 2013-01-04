195a6361ad1bf80e2b8f0419b213501244e9f887
Norman Khine <khinester@aqoon.local>
2013-01-04 18:38:25 +0100

    add more translations

--- js/footer.js
+++ js/footer.js
@@ -1,13 +1,16 @@
 jQuery(function($) {
     language_complete = navigator.language.split("-");
     language = (language_complete[0]);
+    //language = 'fr';
     console.log("I speak (root): %s", language);
 
     i18n.init({ lng: language, debug: true }, function() {
         // save to use translation function as resources are fetched
         $(".tzm-i18n").i18n();
         $(".menu-i18n").i18n();
+        $(".user-i18n").i18n();
         $(".search-i18n").i18n();
+        $(".footer-i18n").i18n();
     });
     // footer navigation
     var slide = false;
